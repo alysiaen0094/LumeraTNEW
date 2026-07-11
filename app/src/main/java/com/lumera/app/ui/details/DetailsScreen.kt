a03ae79212c7245f8ebf4b2a88fab6752868bcba
barlowd55 <94359831+barlowd55@users.noreply.github.com>
2026-07-11 13:50:18 +0530

    Update DetailsScreen.kt

--- app/src/main/java/com/lumera/app/ui/details/DetailsScreen.kt
+++ app/src/main/java/com/lumera/app/ui/details/DetailsScreen.kt
@@ -447,7 +447,8 @@ fun DetailsScreen(
                     text = currentMovie.description ?: "",
                     style = MaterialTheme.typography.bodyMedium.copy(
                         fontSize = 15.5.sp,
-                        lineHeight = 19.5.sp
+                        lineHeight = 19.5.sp,
+                        fontWeight = FontWeight.Medium
                     ),
                     color = textColor.copy(alpha = 0.86f),
                     maxLines = 5,
